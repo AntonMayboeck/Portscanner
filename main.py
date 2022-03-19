c236bb5b43ea22e418a49f484da12cbfc98eade3
AntonMayboeck <antonmayboeck@gmail.com>
2022-03-19 20:53:18 +0000

    made some changes to the code

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import csv
 
+import random
 import pyfiglet
 import sys
 import socket
 from datetime import datetime
 from scapy.all import *
+import ipaddress
 from scapy.layers.inet import ICMP, IP, TCP, UDP
 
+random.seed(344)
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
 print(ascii_banner)
-ip = "0.0.0.0"
-tcp_ports = [80, 443, 53, 22, 1723,
+
+ip = [x for x in ipaddress.IPv4Network('31.209.224.0/20')]
+random.shuffle(ip)
+most_common_tcp_ports = [80, 443, 53, 22, 1723,
          5060, 8080, 21, 4567, 25,
          3389, 8081, 8000, 110, 10000,
          23, 81, 5000, 8082, 445,
          143, 111, 567, 3306, 110,
          135, 139, 993, 995, 5900]
 
-udp_ports = [161, 123, 53, 500, 111,
+most_common_udp_ports = [161, 123, 53, 500, 111,
              137, 69, 5353]
 
-open_ports = []
+q1 = []
+q2 = []
+q3 = []
 
 def checkhost():
-    ping = IP(dst=ip)/ICMP()
-    res = sr1(ping,timeout=1,verbose=0)
-    if res == None:
-        print("This host is down")
-    else:
-        print("This host is up")
+    for address in ip:
+        ping = IP(dst=address)/ICMP()
+        res = sr1(ping,timeout=1,verbose=0)
+        if res == None:
+            print("This host is down")
+        else:
+            print("This host is up")
 
 
 #function to check open port
@@ -38,22 +46,32 @@ def checkport():
     f = open("scanning.csv", "w")
     writer = csv.writer(f)
     writer.writerow(header)
-    for port in tcp_ports:
-        timer = datetime.now()
-        tcpRequest = IP(dst=ip)/TCP(dport=port, flags="S")
-        tcpResponse = sr1(tcpRequest, timeout=1, verbose=0)
-        try:
-            if tcpResponse.getlayer(TCP).flags == "SA":
-                print(port, "is listening")
-                data = [ip, port, timer, True]
+    for address in ip:
+        counter = 0
+        for port in most_common_tcp_ports:
+            timer = datetime.now()
+            tcpRequest = IP(dst=address)/TCP(dport=port, flags="S")
+            tcpResponse = sr1(tcpRequest, timeout=1, verbose=0)
+            try:
+                if tcpResponse.getlayer(TCP).flags == "SA":
+                    print(port, "is listening")
+                    data = [address, port, timer, True]
+                    writer.writerow(data)
+                    counter += 1
+                if tcpResponse.getlayer(TCP).flags == "RA":
+                    print(port, "is not listening")
+                    data = [address, port, timer, False]
+                    writer.writerow(data)
+            except AttributeError:
+                print(port, "is not listening")
+                data = [address, port, timer, False]
                 writer.writerow(data)
-            else:
-                data = [ip, port, timer, False]
-                writer.writerow(data)
-        except AttributeError:
-            print(port, "is not listening")
-            data = [ip, port, timer, False]
-            writer.writerow(data)
+        if counter >= 5:
+            q1.append(address)
+        elif 5 > counter > 0:
+            q2.append(address)
+        else:
+            q3.append(address)
 
 
 
